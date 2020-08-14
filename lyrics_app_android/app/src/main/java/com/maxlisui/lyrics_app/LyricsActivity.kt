4189274c49655540fdc1aa5ea82ad82ec2af571b
Maximilian Suitner <maxlisui@gmail.com>
2020-08-14 23:57:28 +0200

    Display current Playing song

--- lyrics_app_android/app/src/main/java/com/maxlisui/lyrics_app/LyricsActivity.kt
+++ lyrics_app_android/app/src/main/java/com/maxlisui/lyrics_app/LyricsActivity.kt
@@ -3,14 +3,19 @@ package com.maxlisui.lyrics_app
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import com.maxlisui.lyrics_app.helper.SpotifyHelper
 import com.maxlisui.lyrics_app.model.SpotifySong
 
 class LyricsActivity : AppCompatActivity() {
+
+    private lateinit var playingTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lyrics)
         val token = intent.getStringExtra(ACCESS_TOKEN)
+        playingTextView = findViewById(R.id.playingTextView)
         if(token != null) {
             val helper = SpotifyHelper(token, getString(R.string.spotify_base_url)) {onNewSong(it)}
             helper.getCurrentSong()
@@ -19,5 +24,15 @@ class LyricsActivity : AppCompatActivity() {
 
     private fun onNewSong(song: SpotifySong) {
         Log.d(LYRICS_ACTIVITY_LOG_HELPER, song.currentlyPlayingType)
+        if(song.isPlaying && song.currentlyPlayingType == "track") {
+            var displayValue = song.name
+            if(song.artists.count() > 0) {
+                displayValue += "\n" + song.artists.joinToString(" / ", transform = {it.name})
+            }
+
+            runOnUiThread {
+                playingTextView.text = displayValue
+            }
+        }
     }
 }
