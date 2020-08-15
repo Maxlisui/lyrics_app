4c2c0c478352a18a194f10d4ecc385a68ca58597
Maximilian Suitner <maxlisui@gmail.com>
2020-08-15 22:14:05 +0200

    Lock updating the Song

--- lyrics_app_android/app/src/main/java/com/maxlisui/lyrics_app/helper/SpotifyThread.kt
+++ lyrics_app_android/app/src/main/java/com/maxlisui/lyrics_app/helper/SpotifyThread.kt
@@ -7,21 +7,25 @@ import com.maxlisui.lyrics_app.SpotifySongToVoidAction
 import java.util.concurrent.CountDownLatch
 
 class SpotifyThread(private val spotifyHelper: SpotifyHelper, private val onNewSong: SpotifySongToVoidAction) : Runnable {
+
+    private var currentlyPlayingId = ""
+    private var isPlaying = false
+
     override fun run() {
         Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND)
         try {
-            var currentlyPlayingId = ""
-            var isPaying = false
             while(true) {
                 try {
                     val count = CountDownLatch(1)
                     spotifyHelper.getCurrentSong {
-                        if(currentlyPlayingId != it.id || isPaying != it.isPlaying) {
-                            currentlyPlayingId = it.id
-                            isPaying = it.isPlaying
-                            onNewSong(it)
+                        synchronized(SPOTIFY_THREAD_LOG_HELPER) {
+                            if(currentlyPlayingId != it.id || isPlaying != it.isPlaying) {
+                                currentlyPlayingId = it.id
+                                isPlaying = it.isPlaying
+                                onNewSong(it)
+                            }
+                            count.countDown()
                         }
-                        count.countDown()
                     }
                     count.await()
                     Thread.sleep(2000)
