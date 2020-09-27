00ec1a0fed9d18a47ac062aa533e7ab12d253d5c
Maximilian Suitner <maxlisui@gmail.com>
2020-09-27 21:50:27 +0200

    Added IOS Project

---
+++ lyrics_app_ios/lyrics_app_ios/ViewController.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    // Mark: Actions
+    @IBAction func logn_click(_ sender: UIButton) {
+        
+    }
+    
+}
+
