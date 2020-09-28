449c2ef4043f1c2325b5aaa31e6c3a1a255cbca7
Maximilian Suitner <maxlisui@gmail.com>
2020-09-28 20:27:36 +0200

    [IOS] Added Login button

--- lyrics_app_ios/lyrics_app_ios/AppDelegate.swift
+++ lyrics_app_ios/lyrics_app_ios/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SpotifyLogin
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        SpotifyLogin.shared.configure(clientID: Constants.spotify_client_id, clientSecret: Constants.client_secret, redirectURL: Constants.spotify_redirect_url)
+        
         return true
     }
 
@@ -30,6 +34,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        let handled = SpotifyLogin.shared.applicationOpenURL(url) { (error) in }
+        return handled
     }
 
 
--- lyrics_app_ios/lyrics_app_ios/ViewController.swift
+++ lyrics_app_ios/lyrics_app_ios/ViewController.swift
@@ -1,16 +1,32 @@
 import UIKit
+import SpotifyLogin
 
 class ViewController: UIViewController {
 
+    var loginButton: UIButton?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
-    // Mark: Actions
-    @IBAction func logn_click(_ sender: UIButton) {
         
+        let button = SpotifyLoginButton(viewController: self, scopes: [.userReadPlaybackState, .userReadCurrentlyPlaying])
+        self.loginButton = button
+        self.view.addSubview(button)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(loginSucessful), name: .SpotifyLoginSuccessful, object: nil)
     }
     
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        loginButton?.center = self.view.center
+    }
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc func loginSucessful() {
+        print("YEAH")
+    }
 }
 
