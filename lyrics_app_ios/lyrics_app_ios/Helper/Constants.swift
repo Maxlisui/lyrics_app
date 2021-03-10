e7785fd5c363e2c20f8bd1ccdd099ceac694f808
Maximilian Suitner <maxlisui@gmail.com>
2021-03-10 20:14:16 +0100

    [IOS] Actually include Constants

---
+++ lyrics_app_ios/lyrics_app_ios/Helper/Constants.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+struct Constants {
+    static var spotifyBaseUrl: String = ""
+    static var spotifyClientID: String = ""
+    static var spotifyClientSecret: String = ""
+    static var spotifyRedirectURL: URL = URL.init(string: "")!
+    static var geniusAccessToken: String = ""
+    static var geniusBaseUrl: String = ""
+}
